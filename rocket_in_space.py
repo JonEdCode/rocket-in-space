71dd89a4333b6540d8c48f948f4cedd73c5a6719
JonEdCode <monika_gouws@yahoo.co.uk>
2025-04-07 18:46:14 +0100

    Add files via upload

--- rocket_in_space.py
+++ rocket_in_space.py
@@ -8,15 +8,50 @@ pygame.display.set_caption(TITLE)
 rocket = pygame.image.load("rocket.png")
 space_background = pygame.image.load("space background!.png")
 run = True
-
+x = 300
+y = 300
 while run == True:
     screen.blit(space_background,(0,0)) 
-    screen.blit(rocket,(300,300))
+    screen.blit(rocket,(x,y))
+    #press and hold the key will work
+    key_pressed = pygame.key.get_pressed() 
+    if key_pressed[pygame.K_LEFT]:
+        x = x -10
+    if key_pressed[pygame.K_RIGHT]:
+        x = x +10 
+    if key_pressed[pygame.K_UP]:
+        y = y -10
+    if key_pressed[pygame.K_DOWN]:
+        y = y +10
     
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run = False 
+            #press and moving not work
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x = x -10
+            if event.key == pygame.K_RIGHT:
+                x = x +10
+            if event.key == pygame.K_UP:
+                y = y -10
+            if event.key == pygame.K_DOWN:
+                y = y +10
+
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+    if x > 400:
+        x = 400
+    if y > 400:
+        y = 400
+    
+    
+    
+
+
 
     pygame.display.update()
 
